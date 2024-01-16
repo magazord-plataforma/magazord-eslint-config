f816b3e7760ee7d412837cedd8f96ecd6ea067ed
Gabriel Schmidt Cordeiro <gabriel.cordeiro@magazord.com.br>
2024-01-16 10:25:26 -0300

    fix: 🐛 Correções no Script

--- index.js
+++ index.js
@@ -1,53 +1,44 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    plugins: ['@typescript-eslint', 'react'],
-    rules: {
-        'prettier/prettier': [
-            'error',
-            {
-                tabWidth: 4,
-                singleQuote: true,
-                trailingComma: 'all',
-                arrowParens: 'always',
-                semi: false,
-                endOfLine: 'auto',
-                jsxSingleQuote: false,
-                jsxBracketSameLine: false,
-                bracketSpacing: true,
-            },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
         ],
-        '@typescript-eslint/no-explicit-any': ['off'],
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-uses-react': 'off',
-        'react/jsx-key': 'off',
-        'no-tabs': 'error',
-        // Remova a propriedade overrides do bloco rules
+        "quotes": [
+            "error",
+            "single",
+            { "allowTemplateLiterals": true }
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "@typescript-eslint/no-explicit-any": ["off"],
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-uses-react": "off",
+        "react/jsx-key": "off",
+        "no-tabs": "error",
+        "jsx-quotes": ["error", "prefer-double"],
     },
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            rules: {
-                // Configurações específicas para TypeScript aqui
-                semi: ['error', 'never'],
-                singleQuote: ['error', 'true'], // ou 'error', dependendo da sua preferência
-                tabWidth: 4,
-                jsxSingleQuote: false,
-            },
-        },
-    ],
-    ignorePatterns: ['node_modules'],
-};
+    "ignorePatterns": [
+        "node_modules"
+    ]
+}
