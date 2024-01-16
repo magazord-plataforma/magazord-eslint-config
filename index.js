c9c2599a2bdfef93d89efe3c05cfbddf4f379023
Gabriel Schmidt Cordeiro <gabriel.cordeiro@magazord.com.br>
2024-01-16 09:47:00 -0300

    feat: 🎸 Alterações nas configurações de espaçamento

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
-        "prettier/prettier": [
-            "error", {
-                "tabWidth": 4,
-                "singleQuote": true,
-                "trailingComma": 'all',
-                "arrowParens": 'always',
-                "semi": false,
-                'endOfLine': 'auto',
-                "jsxSingleQuote": false,
-                "jsxBracketSameLine": false,
-                "bracketSpacing": true,
-                "overrides": [
-                    {
-                        "files": ["*.ts", "*.tsx"],
-                        "options": {
-                            "semi": false,
-                            "singleQuote": true,
-                            "tabWidth": 4,
-                            "jsxSingleQuote": false
-                        }
-                    }
-                ]
-            }
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+                singleQuote: true,
+                trailingComma: 'all',
+                arrowParens: 'always',
+                semi: false,
+                endOfLine: 'auto',
+                jsxSingleQuote: false,
+                jsxBracketSameLine: false,
+                bracketSpacing: true,
+            },
         ],
-        "@typescript-eslint/no-explicit-any": ["off"],
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-uses-react": "off",
-        "react/jsx-key": "off",
-        "no-tabs": "error",
+        '@typescript-eslint/no-explicit-any': ['off'],
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/jsx-key': 'off',
+        'no-tabs': 'error',
     },
-    "ignorePatterns": [
-        "node_modules"
-    ]
-}
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            options: {
+                semi: false,
+                singleQuote: true,
+                tabWidth: 4,
+                jsxSingleQuote: false,
+            },
+        },
+    ],
+    ignorePatterns: ['node_modules'],
+};
